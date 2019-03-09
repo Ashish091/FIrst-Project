af4594153a48ae0e9b0b38cf28e903f40ed3def2
Ashish091 <ashish.professional91@gmail.com>
2019-03-09 11:46:34 +0530

    Added display method.

---
+++ src/core/Calculator.java
@@ -0,0 +1,18 @@
+package core;
+
+public class Calculator {
+
+	 public void hi()
+	 {
+		 
+		System.out.println("Hi !!!!"); 
+	 }
+	public static void main(String[] args) {
+		
+		Calculator c= new Calculator();
+		c.hi();
+		
+     
+	}
+
+}
